41b1b5f1d78e63ccedb9170538f6139a4d9f6a21
Alex Henning <elcron@gmail.com>
2010-10-13 19:44:02 -0400

    Cleaned up the documentation.

--- examples/madlib.py
+++ examples/madlib.py
@@ -60,7 +60,8 @@ class Madlib(object):
 
 if __name__ == "__main__":
     m = Madlib("People are often surprised, somtimes astounded at the skill he excersized. He could run, walk, sleep and even talk!", 4)
-
+    m = Madlib("""The Cylon War is long over, yet we must not forget the reasons why so many sacrificed so much in the cause of freedom. The cost of wearing the uniform can be high ... [after looking at crowd] but sometimes it's too high.
+You know, when we fought the Cylons, we did it to save ourselves from extinction. But we never answered the question, why? Why are we as a people worth saving? We still commit murder because of greed, spite, jealousy. And we still visit all of our sins upon our children. We refuse to accept the responsibility for anything that we've done. Like we did with the Cylons. We decided to play God, create life. When that life turned against us, we comforted ourselves in the knowledge that it really wasn't our fault, not really. You cannot play God then wash your hands of the things that you've created. Sooner or later, the day comes when you can't hide from the things that you've done anymore.""", 10)
     """After the war the army was scraping the bottom of the barrel to get the guys for the occupation forces in Germany. Up until then the army
 deferred people for some reason other than physical first (I was deferred because I was working on the bomb), but now they reversed that and gave
 everybody a physical first."""
@@ -75,6 +76,9 @@ at one, your hearing at another, they take your blood sample at another, and so 
 Anyway, finally you come to booth number thirteen: psychiatrist. There you wait, sitting on one of the benches, and while I'm waiting I can see
 what is happening. There are three desks, with a psychiatrist behind each one, and the "culprit" sits across from the psychiatrist in his BVDs and
 answers various questions."""
+    print ""
     while m.next() != None:
         m.answer_next(raw_input("Please give me %s. "%m.next()))
+
+    print ""
     print m
